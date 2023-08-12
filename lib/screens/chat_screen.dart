486bf51076592a4a5f5648ca7309c394b24ab1d7
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-13 03:51:48 +0500

    wp ui

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -6,40 +6,60 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          leading: const Icon(Icons.whatsapp_outlined),
-          title: const Text('WhatsApp'),
-          actions: [
-            const Icon(Icons.search_outlined),
-            const SizedBox(width: 10,),
-            const Icon(Icons.camera_alt_outlined),
-            const SizedBox(width: 10,),
-            PopupMenuButton(
-              itemBuilder: (context) => [
-                const PopupMenuItem(
-                  value: 1,
-                  child: Text('Auto Reply'),
-                ),
-                const PopupMenuItem(
-                  value: 2,
-                  child: Text('Message a number'),
-                ),
-                const PopupMenuItem(
-                  value: 3,
-                  child: Text('New Groups'),
-                ),
-                const PopupMenuItem(
-                  value: 4,
-                  child: Text('Linked devices'),
-                ),
-                const PopupMenuItem(
-                  value: 5,
-                  child: Text('Settings'),
-                ),
-              ],
-            )
-          ],
+      child: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            leading: const Icon(Icons.whatsapp_outlined),
+            title: const Text(
+              'WhatsApp',
+              textAlign: TextAlign.start,
+            ),
+            actions: [
+              const Icon(Icons.search_outlined),
+              const SizedBox(width: 10),
+              const Icon(Icons.camera_alt_outlined),
+              const SizedBox(width: 10),
+              PopupMenuButton(
+                itemBuilder: (context) => [
+                  const PopupMenuItem(
+                    value: 1,
+                    child: Text('Auto Reply'),
+                  ),
+                  const PopupMenuItem(
+                    value: 2,
+                    child: Text('Message a number'),
+                  ),
+                  const PopupMenuItem(
+                    value: 3,
+                    child: Text('New Groups'),
+                  ),
+                  const PopupMenuItem(
+                    value: 4,
+                    child: Text('Linked devices'),
+                  ),
+                  const PopupMenuItem(
+                    value: 5,
+                    child: Text('Settings'),
+                  ),
+                ],
+              )
+            ],
+            bottom: const TabBar(tabs: [
+              Tab(
+                child: Text('Chats'),
+              ),
+              Tab(
+                child: Text('Groups'),
+              ),
+              Tab(
+                child: Text('Status'),
+              ),
+              Tab(
+                child: Text('Calls'),
+              ),
+            ]),
+          ),
         ),
       ),
     );
