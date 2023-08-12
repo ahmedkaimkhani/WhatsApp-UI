e9ffb43c40a56e6e2954f8c45dce2682a7841fa2
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-13 03:17:29 +0500

    wp ui

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -9,6 +9,37 @@ class ChatScreen extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           leading: const Icon(Icons.whatsapp_outlined),
+          title: const Text('WhatsApp'),
+          actions: [
+            const Icon(Icons.search_outlined),
+            const SizedBox(width: 10,),
+            const Icon(Icons.camera_alt_outlined),
+            const SizedBox(width: 10,),
+            PopupMenuButton(
+              itemBuilder: (context) => [
+                const PopupMenuItem(
+                  value: 1,
+                  child: Text('Auto Reply'),
+                ),
+                const PopupMenuItem(
+                  value: 2,
+                  child: Text('Message a number'),
+                ),
+                const PopupMenuItem(
+                  value: 3,
+                  child: Text('New Groups'),
+                ),
+                const PopupMenuItem(
+                  value: 4,
+                  child: Text('Linked devices'),
+                ),
+                const PopupMenuItem(
+                  value: 5,
+                  child: Text('Settings'),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
