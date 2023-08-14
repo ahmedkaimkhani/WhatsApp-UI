14f4bb646e100344ef96e694bcf3f73a5d297a68
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-14 19:00:57 +0500

    whatsapp ui

--- lib/screens/group_screen.dart
+++ lib/screens/group_screen.dart
@@ -6,15 +6,83 @@ class GroupScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: const [
-        CustomGroupStyle(
-          name: 'SMIT0011-Flutter-2023',
-          subtitle: '~ <S-M-M-S>: W8 for end',
-          time: '12:31 AM',
-          image: 'assets/images/smit.png',
-        ),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: const [
+          CustomGroupStyle(
+            name: 'SMIT0011-Flutter-2023',
+            subtitle: '~ <S-M-M-S>: W8 for end',
+            time: '12:31 AM',
+            image: 'assets/images/smit.png',
+          ),
+          CustomGroupStyle(
+            name: 'The Boys ( Flutter )',
+            subtitle: 'Mohsin: tumhy bhejunga uganda',
+            time: '12:29 PM',
+            image: 'assets/images/flutter.png',
+          ),
+          CustomGroupStyle(
+            name: 'Flop Plansss',
+            subtitle: 'Sufyan: Meri to roh parvaz kar gaii bha...',
+            time: '2:10 PM',
+            image: 'assets/images/flop.png',
+          ),
+          CustomGroupStyle(
+            name: 'SMIT0011-Flutter-2023',
+            subtitle: '~ <S-M-M-S>: W8 for end',
+            time: '12:31 AM',
+            image: 'assets/images/smit.png',
+          ),
+          CustomGroupStyle(
+            name: 'The Boys ( Flutter )',
+            subtitle: 'Mohsin: tumhy bhejunga uganda',
+            time: '12:29 PM',
+            image: 'assets/images/flutter.png',
+          ),
+          CustomGroupStyle(
+            name: 'Flop Plansss',
+            subtitle: 'Sufyan: Meri to roh parvaz kar gaii bha...',
+            time: '2:10 PM',
+            image: 'assets/images/flop.png',
+          ),
+          CustomGroupStyle(
+            name: 'SMIT0011-Flutter-2023',
+            subtitle: '~ <S-M-M-S>: W8 for end',
+            time: '12:31 AM',
+            image: 'assets/images/smit.png',
+          ),
+          CustomGroupStyle(
+            name: 'The Boys ( Flutter )',
+            subtitle: 'Mohsin: tumhy bhejunga uganda',
+            time: '12:29 PM',
+            image: 'assets/images/flutter.png',
+          ),
+          CustomGroupStyle(
+            name: 'Flop Plansss',
+            subtitle: 'Sufyan: Meri to roh parvaz kar gaii bha...',
+            time: '2:10 PM',
+            image: 'assets/images/flop.png',
+          ),
+          CustomGroupStyle(
+            name: 'SMIT0011-Flutter-2023',
+            subtitle: '~ <S-M-M-S>: W8 for end',
+            time: '12:31 AM',
+            image: 'assets/images/smit.png',
+          ),
+          CustomGroupStyle(
+            name: 'The Boys ( Flutter )',
+            subtitle: 'Mohsin: tumhy bhejunga uganda',
+            time: '12:29 PM',
+            image: 'assets/images/flutter.png',
+          ),
+          CustomGroupStyle(
+            name: 'Flop Plansss',
+            subtitle: 'Sufyan: Meri to roh parvaz kar gaii bha...',
+            time: '2:10 PM',
+            image: 'assets/images/flop.png',
+          ),
+        ],
+      ),
     );
   }
 }
