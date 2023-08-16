748beb26fb7fb4fc1a180bcb775ad2e666f833c6
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-17 01:15:30 +0500

    ui

--- lib/Styling/styling.dart
+++ lib/Styling/styling.dart
@@ -6,8 +6,9 @@ class CustomColor {
 }
 
 class Styling {
-  static const font_16 = TextStyle(
-    fontSize: 16,
-  );
+  static const font_16 = TextStyle(fontSize: 16);
   static const font_13 = TextStyle(fontSize: 13);
+
+  static const font_14 = TextStyle(fontSize: 14);
+  static const font_12 = TextStyle(fontSize: 12);
 }
