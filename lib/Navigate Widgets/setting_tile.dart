94fbda2fe02016ee3eea640e9784fe122c428111
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-17 01:21:05 +0500

    ui

---
+++ lib/Navigate Widgets/setting_tile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomSettingStyle extends StatelessWidget {
+  const CustomSettingStyle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
