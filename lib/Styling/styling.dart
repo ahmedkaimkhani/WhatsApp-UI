01a5ce5f7c190dede8faadd06fc56b15c315891f
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-17 01:11:31 +0500

    ui

---
+++ lib/Styling/styling.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static const colorRecentStatus = Colors.teal;
+  static const colorViewedStatus = Colors.grey;
+}
+
+class Styling {
+  static const font_16 = TextStyle(
+    fontSize: 16,
+  );
+  static const font_13 = TextStyle(fontSize: 13);
+}
