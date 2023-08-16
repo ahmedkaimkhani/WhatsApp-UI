e03277729cc17ab320825d0bf546ee9fdc281fba
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-17 04:26:00 +0500

    ui

--- lib/Navigate Screens/settings.dart
+++ lib/Navigate Screens/settings.dart
@@ -7,36 +7,86 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: const Icon(Icons.arrow_back)),
-        title: const Text('Settings'),
-        actions: const [
-          Padding(
-            padding: EdgeInsets.only(right: 15),
-            child: Icon(Icons.search),
-          )
-        ],
-      ),
-      body: Column(
-        children: [
-          customListTile(
-            title: 'Ahmed Kaim Khani',
-            subtitle: 'Chasing goals.',
-            image: 'assets/images/mine.jpg',
-            icon: Icons.qr_code,
-            fontSizeT: Styling.font_18,
-            fontSizeS: Styling.font_14,
-          ),
-          const Divider(
-            color: Colors.grey,
-            thickness: 0.1,
-          ),
-        ],
+    return SingleChildScrollView(
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(Icons.arrow_back)),
+          title: const Text('Settings'),
+          actions: const [
+            Padding(
+              padding: EdgeInsets.only(right: 15),
+              child: Icon(Icons.search),
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: customListTile(
+                title: 'Ahmed Kaim Khani',
+                subtitle: 'Chasing goals.',
+                image: 'assets/images/mine.jpg',
+                iconT: Icons.qr_code,
+                fontSizeT: Styling.font_18,
+                fontSizeS: Styling.font_14,
+              ),
+            ),
+            const Divider(
+              color: Colors.grey,
+              thickness: 0.1,
+            ),
+            customListTile(
+              title: 'Account',
+              subtitle: 'Security notifications, chnage\nnumber',
+              icon: Icons.key,
+            ),
+            customListTile(
+              title: 'Privacy',
+              subtitle: 'Block contacts, disappearing\nmessages',
+              icon: Icons.lock,
+            ),
+            customListTile(
+              title: 'Avatar',
+              subtitle: 'Create, edit, profile photo',
+              icon: Icons.account_circle,
+            ),
+            customListTile(
+              title: 'Chats',
+              subtitle: 'Theme, wallpapers, chat history',
+              icon: Icons.chat,
+            ),
+            customListTile(
+              title: 'Notifications',
+              subtitle: 'Message, group & call tones',
+              icon: Icons.notifications,
+            ),
+            customListTile(
+              title: 'Storage and data',
+              subtitle: 'Network usage, auto-download',
+              icon: Icons.data_saver_off,
+            ),
+            customListTile(
+              title: 'App language',
+              subtitle: 'English (phone\'s language)',
+              icon: Icons.language,
+            ),
+            customListTile(
+              title: 'Help',
+              subtitle: 'help center, contactus, privacy ploicy',
+              icon: Icons.help_outline,
+            ),
+            customListTile(
+              title: 'Invite a friend',
+              subtitle: '',
+              icon: Icons.group,
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/Navigate Widgets/setting_tile.dart
+++ lib/Navigate Widgets/setting_tile.dart
@@ -6,34 +6,38 @@ Widget customListTile({
   required String subtitle,
   String? image,
   IconData? icon,
+  IconData? iconT,
   String? text,
   TextStyle? fontSizeT,
   TextStyle? fontSizeS,
 }) {
-  return Padding(
-    padding: const EdgeInsets.only(top: 10),
-    child: ListTile(
-      leading: image != null
-          ? CircleAvatar(
-              radius: 40,
-              backgroundImage: AssetImage(image),
-            )
-          : Icon(icon),
-      title: Text(
+  return ListTile(
+    leading: image != null
+        ? CircleAvatar(
+            radius: 40,
+            backgroundImage: AssetImage(image),
+          )
+        : Icon(
+            icon,
+            color: Colors.grey,
+          ),
+    title: Padding(
+      padding: const EdgeInsets.only(bottom: 5),
+      child: Text(
         title,
         style: fontSizeT != null ? Styling.font_18 : Styling.font_14,
       ),
-      subtitle: Text(
-        subtitle,
-        style: fontSizeS != null ? Styling.font_14 : Styling.font_12,
-      ),
-      trailing: text != null
-          ? Text(text)
-          : Icon(
-              icon,
-              size: 30,
-              color: Colors.teal,
-            ),
     ),
+    subtitle: Text(
+      subtitle,
+      style: fontSizeS != null ? Styling.font_14 : Styling.font_12,
+    ),
+    trailing: text != null
+        ? Text(text)
+        : Icon(
+            iconT,
+            size: 30,
+            color: Colors.teal,
+          ),
   );
 }
