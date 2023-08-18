8c5691a3f803c8d24d42471b803760ab39bb88c4
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-19 04:46:49 +0500

    ui

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -77,107 +77,122 @@ class ChatScreen extends StatelessWidget {
               ),
             ]),
           ),
-          body: TabBarView(
+          body: Stack(
             children: [
-              // Chat Screen
-              SingleChildScrollView(
-                child: Column(
-                  children: const [
-                    CustomChatStyle(
-                      name: 'Ahmed',
-                      subtitle: 'set hai',
-                      time: '12:00 AM',
-                      image: null,
-                      icon: 'unseen',
+              TabBarView(
+                children: [
+                  // Chat Screen
+                  SingleChildScrollView(
+                    child: Column(
+                      children: const [
+                        CustomChatStyle(
+                          name: 'Ahmed',
+                          subtitle: 'set hai',
+                          time: '12:00 AM',
+                          image: null,
+                          icon: 'unseen',
+                        ),
+                        CustomChatStyle(
+                          name: 'Asim',
+                          subtitle: 'ok jani',
+                          time: '8:33 PM',
+                          image: 'assets/images/asim.jpg',
+                          icon: 'seen',
+                        ),
+                        CustomChatStyle(
+                          name: 'Usman',
+                          subtitle: 'thek hai',
+                          time: '5:53 PM',
+                          image: 'assets/images/usman.jpg',
+                          icon: 'unseen',
+                        ),
+                        CustomChatStyle(
+                          name: 'Arbaz',
+                          subtitle: 'ok bro',
+                          time: '10:14 PM',
+                          image: 'assets/images/arbaz.jpg',
+                          icon: 'unseen',
+                        ),
+                        CustomChatStyle(
+                          name: 'Naveed',
+                          subtitle: 'chalty hain',
+                          time: '8:33 PM',
+                          image: 'assets/images/naveed.jpg',
+                        ),
+                        CustomChatStyle(
+                          name: 'Areesh',
+                          subtitle: 'bethty hain',
+                          time: '6:44 PM',
+                          icon: 'seen',
+                        ),
+                        CustomChatStyle(
+                          name: 'Awais',
+                          subtitle: 'krty hain kuch',
+                          time: '2:35 AM',
+                          image: 'assets/images/awais.jpg',
+                          icon: 'seen',
+                        ),
+                        CustomChatStyle(
+                          name: 'Faish SMIT',
+                          subtitle: 'ok',
+                          time: '3:11 AM',
+                          icon: 'seen',
+                        ),
+                        CustomChatStyle(
+                          name: 'Hamza',
+                          subtitle: 'best hai',
+                          time: '6:47 PM',
+                          image: 'assets/images/hamza.jpg',
+                        ),
+                        CustomChatStyle(
+                          name: 'Saim',
+                          subtitle: 'hmm',
+                          time: '1:52 AM',
+                          image: 'assets/images/saim.jpg',
+                          icon: 'unseen',
+                        ),
+                        CustomChatStyle(
+                          name: 'Imran',
+                          subtitle: 'sahi',
+                          time: '3:16 PM',
+                          image: 'assets/images/imran.jpg',
+                          icon: 'unseen',
+                        ),
+                        CustomChatStyle(
+                          name: 'Kashif',
+                          subtitle: 'chalain',
+                          time: '7:46 PM',
+                          image: 'assets/images/kashif.jpg',
+                          icon: 'seen',
+                        ),
+                        CustomChatStyle(
+                          name: 'Faizan',
+                          subtitle: 'aajaaa',
+                          time: '5:15 PM',
+                          image: 'assets/images/faizan.jpg',
+                        ),
+                      ],
                     ),
-                    CustomChatStyle(
-                      name: 'Asim',
-                      subtitle: 'ok jani',
-                      time: '8:33 PM',
-                      image: 'assets/images/asim.jpg',
-                      icon: 'seen',
-                    ),
-                    CustomChatStyle(
-                      name: 'Usman',
-                      subtitle: 'thek hai',
-                      time: '5:53 PM',
-                      image: 'assets/images/usman.jpg',
-                      icon: 'unseen',
-                    ),
-                    CustomChatStyle(
-                      name: 'Arbaz',
-                      subtitle: 'ok bro',
-                      time: '10:14 PM',
-                      image: 'assets/images/arbaz.jpg',
-                      icon: 'unseen',
-                    ),
-                    CustomChatStyle(
-                      name: 'Naveed',
-                      subtitle: 'chalty hain',
-                      time: '8:33 PM',
-                      image: 'assets/images/naveed.jpg',
-                    ),
-                    CustomChatStyle(
-                      name: 'Areesh',
-                      subtitle: 'bethty hain',
-                      time: '6:44 PM',
-                      icon: 'seen',
-                    ),
-                    CustomChatStyle(
-                      name: 'Awais',
-                      subtitle: 'krty hain kuch',
-                      time: '2:35 AM',
-                      image: 'assets/images/awais.jpg',
-                      icon: 'seen',
-                    ),
-                    CustomChatStyle(
-                      name: 'Faish SMIT',
-                      subtitle: 'ok',
-                      time: '3:11 AM',
-                      icon: 'seen',
-                    ),
-                    CustomChatStyle(
-                      name: 'Hamza',
-                      subtitle: 'best hai',
-                      time: '6:47 PM',
-                      image: 'assets/images/hamza.jpg',
-                    ),
-                    CustomChatStyle(
-                      name: 'Saim',
-                      subtitle: 'hmm',
-                      time: '1:52 AM',
-                      image: 'assets/images/saim.jpg',
-                      icon: 'unseen',
-                    ),
-                    CustomChatStyle(
-                      name: 'Imran',
-                      subtitle: 'sahi',
-                      time: '3:16 PM',
-                      image: 'assets/images/imran.jpg',
-                      icon: 'unseen',
-                    ),
-                    CustomChatStyle(
-                      name: 'Kashif',
-                      subtitle: 'chalain',
-                      time: '7:46 PM',
-                      image: 'assets/images/kashif.jpg',
-                      icon: 'seen',
-                    ),
-                    CustomChatStyle(
-                      name: 'Faizan',
-                      subtitle: 'aajaaa',
-                      time: '5:15 PM',
-                      image: 'assets/images/faizan.jpg',
-                    ),
-                  ],
-                ),
+                  ),
+                  // 2: Group Screen
+                  const GroupScreen(),
+                  // 3: Status Screen
+                  const StatusScreen(),
+                  // 4: Call Screen
+                  const CallScreen(),
+                ],
               ),
-              // 2: Group Screen
-              const GroupScreen(),
-              // 3: Status Screen
-              const StatusScreen(),
-              // 4: Call Screen
-              const CallScreen(),
+              Positioned(
+                  right: 20,
+                  bottom: 50,
+                  child: CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Colors.deepOrange.shade400,
+                    child: const Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    ),
+                  )),
             ],
           ),
         ),
