424b8fdb29a42b01fa4dad4ee19959b20cee2ad0
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-18 16:06:18 +0500

    ui

--- lib/Navigate Widgets/profile_tile.dart
+++ lib/Navigate Widgets/profile_tile.dart
@@ -20,58 +20,62 @@ class CustomProfileTile extends StatelessWidget {
       width: double.infinity,
       color: Colors.red,
       child: Row(
-        children: [],
+        children: [
+          Icon(iconL),
+          Column(
+            children: [],
+          )
+        ],
       ),
     );
   }
 }
 
-
 // Column(
-      //   children: [
-      //     Row(
-      //       children: [
-      //         Icon(iconL),
-      //         Column(
-      //           children: [
-      //             Text(
-      //               subTitle,
-      //               style: TextStyle(fontSize: 12, color: Colors.grey),
-      //             ),
-      //             Text(
-      //               title,
-      //               style: TextStyle(fontSize: 14, color: Colors.white),
-      //             ),
-      //             Icon(iconR),
-      //           ],
-      //         )
-      //       ],
-      //     )
-      //   ],
-      // ),
+//   children: [
+//     Row(
+//       children: [
+//         Icon(iconL),
+//         Column(
+//           children: [
+//             Text(
+//               subTitle,
+//               style: TextStyle(fontSize: 12, color: Colors.grey),
+//             ),
+//             Text(
+//               title,
+//               style: TextStyle(fontSize: 14, color: Colors.white),
+//             ),
+//             Icon(iconR),
+//           ],
+//         )
+//       ],
+//     )
+//   ],
+// ),
 
-      // ListTile(
-      //   leading: Icon(
-      //     iconL,
-      //     size: 18,
-      //     color: Colors.grey,
-      //   ),
-      //   title: Row(
-      //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      //     children: [
-      //       Text(
-      //         title,
-      //         style: TextStyle(fontSize: 12, color: Colors.grey),
-      //       ),
-      //       Icon(
-      //         iconR,
-      //         size: 18,
-      //         color: Colors.teal,
-      //       ),
-      //     ],
-      //   ),
-      //   subtitle: Padding(
-      //     padding: const EdgeInsets.only(top: 5),
-      //     child: Text(subTitle),
-      //   ),
-      // ),
+// ListTile(
+//   leading: Icon(
+//     iconL,
+//     size: 18,
+//     color: Colors.grey,
+//   ),
+//   title: Row(
+//     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//     children: [
+//       Text(
+//         title,
+//         style: TextStyle(fontSize: 12, color: Colors.grey),
+//       ),
+//       Icon(
+//         iconR,
+//         size: 18,
+//         color: Colors.teal,
+//       ),
+//     ],
+//   ),
+//   subtitle: Padding(
+//     padding: const EdgeInsets.only(top: 5),
+//     child: Text(subTitle),
+//   ),
+// ),
