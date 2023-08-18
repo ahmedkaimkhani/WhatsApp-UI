07d77bd0d18ceed4b2716e183d4e09b61917febf
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-19 03:20:10 +0500

    ui

--- lib/Navigate Screens/profile.dart
+++ lib/Navigate Screens/profile.dart
@@ -48,10 +48,7 @@ class ProfileScreen extends StatelessWidget {
             iconL: Icons.help_outline,
             iconR: Icons.edit,
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 63),
-            child: CustomDivider.divider,
-          ),
+          CustomDivider.divider,
           const CustomProfileTile(
             title: 'Phone',
             subTitle: '+92 3151717101',
